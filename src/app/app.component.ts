fee3afde643d7b0e49fa8f2e021520bee57ca571
jspe2ba04 <jspe2ba04@scs.local>
2025-03-12 09:38:55 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
+import { IonRange, IonRadio, IonButton, IonCheckbox, IonToggle } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [IonToggle, IonRange, IonCheckbox, IonRadio, IonButton],
 })
+
 export class AppComponent {
+  toggles = { option1: false, option2: false, option3: false };
+  rangeValue = 0;
+  checkboxes = { checkbox1: false, checkbox2: false, checkbox3: false };
+  selectedRadio: string | null = null;
+
   constructor() {}
+
+  pinFormatter(value: number): string {
+    return `${value}%`;
+  }
+  
+  async ngOnInit() {
+    await this.getPreferences(); 
+  }
+
+  async setPreferences() {
+    await Preferences.set({
+      key: 'userSettings',
+      value: JSON.stringify({
+        toggles: this.toggles,
+        rangeValue: this.rangeValue,
+        checkboxes: this.checkboxes,
+        selectedRadio: this.selectedRadio,
+      }),
+    });
+  }
+
+  async getPreferences() {
+    const savedSettings = await Preferences.get({ key: 'userSettings' });
+
+    if (savedSettings.value) {
+      const settings = JSON.parse(savedSettings.value);
+      this.toggles = settings.toggles;
+      this.rangeValue = settings.rangeValue;
+      this.checkboxes = settings.checkboxes;
+      this.selectedRadio = settings.selectedRadio;
+    }
+  }
+
+  async resetAllSettings() {
+    this.toggles = { option1: false, option2: false, option3: false };
+    this.rangeValue = 0;
+    this.checkboxes = { checkbox1: false, checkbox2: false, checkbox3: false };
+    this.selectedRadio = null;
+
+    await Preferences.clear;
+  }
 }
